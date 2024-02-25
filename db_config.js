01735e147d540196768978ef240ffe0becef6389
Yash Tripathi <yashtripathi2205@gmail.com>
2024-02-25 12:11:19 +0530

    Defined Code

--- db_config.js
+++ db_config.js
@@ -1,1 +1,19 @@
+const mysql= require("mysql2");
+const connection= mysql.createConnection({
+    host:"127.0.0.1",
+    user: "root",
+    password:"<password>",
+    database: "student",
 
+});
+
+connection.connect((err) =>{
+    if(err){
+        console.log(`Database Error : ${err} `);
+    }
+    else{
+        console.log(`Database connection successfull`);
+    }
+});
+
+module.exports= {connection};
